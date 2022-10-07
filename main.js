d6e597167a7979d3cea1ba06900e0aecb7c3a619
NeelN123 <96849662+NeelN123@users.noreply.github.com>
2022-10-06 20:55:34 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas= createCanvas(1000, 1000);
+    canvas.center(500,500);
+}
+
+function draw(){
+
+}
+function take_snapshot(){
+    save('myFilterImage.png')
+}
